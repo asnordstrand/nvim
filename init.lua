9ed42dbfcb972eb12395efacc2f1bdad3e323051
Anna Sofie Nordstrand <annasofie@nordstrand.dev>
2022-10-28 13:17:20 -0500

    feat: add error notification for unsupported Neovim versions

--- init.lua
+++ init.lua
@@ -1,3 +1,10 @@
+if vim.fn.has('nvim-0.8') ~= 1 then
+  vim.schedule(function()
+    local level = vim.log.levels.WARN
+    vim.notify('Unsupported Neovim version found. Neovim v0.8+ is required.', level)
+  end)
+end
+
 require('settings')
 require('keymappings')
 require('plugins')
